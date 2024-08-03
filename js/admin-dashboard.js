4e0a9027ebfcfad4db5fe4c69ca5c30a506c49fe
archflair <archflair.co@gmail.com>
2024-08-03 12:50:30 +0530

    again updated app script

--- js/admin-dashboard.js
+++ js/admin-dashboard.js
@@ -1,5 +1,5 @@
 // Replace with your Google Apps Script URL
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyqqArAoIHQ24VSH91fyDAeRHpsMzhqlJp_OI7Ej_NmeaYtVejw6m9Wcot_SWpoXBYjSg/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzwou-BRDnseVI0jYKZyEltf5bU-G89xyvOpCwIkgqMCWOQ4h-9JyoYHZD9eBpR-d9QpQ/exec';
 
 document.addEventListener('DOMContentLoaded', loadPendingJobs);
 
--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const jobsPerLoad = 9;
 let loading = false;
 
 // Replace with your Google Apps Script URL
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyqqArAoIHQ24VSH91fyDAeRHpsMzhqlJp_OI7Ej_NmeaYtVejw6m9Wcot_SWpoXBYjSg/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzwou-BRDnseVI0jYKZyEltf5bU-G89xyvOpCwIkgqMCWOQ4h-9JyoYHZD9eBpR-d9QpQ/exec';
 
 // Wait for the DOM to be fully loaded before executing any scripts
 document.addEventListener('DOMContentLoaded', () => {
